07aa2a6403f63583e951263acb99d98280739167
adrianoadacosta <adrianoadacosta@outlook.com>
2023-05-31 00:35:06 -0300

    Correção - dando um tratamento melhor para a promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -123,7 +122,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Nao ha peca para ser promovida");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Tipo invalido para promocao");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
@@ -335,7 +334,7 @@ public class ChessMatch {
 		placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
 		placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
 		placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
-		placeNewPiece('g', 6, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
 		placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
 
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
@@ -347,7 +346,7 @@ public class ChessMatch {
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 7, new Pawn(board, Color.BLACK, this));
-		placeNewPiece('b', 3, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
 		placeNewPiece('c', 7, new Pawn(board, Color.BLACK, this));
 		placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
 		placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
